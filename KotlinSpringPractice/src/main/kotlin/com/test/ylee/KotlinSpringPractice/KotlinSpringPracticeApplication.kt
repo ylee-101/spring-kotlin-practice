d622830a5ac5823e383823eb81ad3a1374bfb716
ylee <138186070+ylee-101@users.noreply.github.com>
2026-09-07 01:32:29 +0000

    set init springboot

---
+++ KotlinSpringPractice/src/main/kotlin/com/test/ylee/KotlinSpringPractice/KotlinSpringPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.test.ylee.KotlinSpringPractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringPracticeApplication>(*args)
+}
---
+++ KotlinSpringPractice/src/test/kotlin/com/test/ylee/KotlinSpringPractice/KotlinSpringPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.ylee.KotlinSpringPractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
