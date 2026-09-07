82de96d8591cfdb6d92bb24c9d1179287084d21d
ylee <138186070+ylee-101@users.noreply.github.com>
2026-09-07 02:10:43 +0000

    add test controller

---
+++ src/main/kotlin/com/test/ylee/KotlinSpringPractice/controller/TestController.kt
@@ -0,0 +1,16 @@
+package com.test.ylee.KotlinSpringPractice
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping("/hello")
+
+    fun hello(): String {
+
+        return "Hello Spring Boot!"
+
+    }
+}
